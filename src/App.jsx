72eebc930e0c925d3ab7f320695a9e3a6131cde6
SHETH MANAV PRASHANT <shethmanav2002@gmail.com>
2023-02-15 17:57:30 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,35 @@ import Event from "./components/Events/Event";
 import Sponsor from "./components/Sponsor/Sponsor";
 import { NavBar } from "./components/Nav/NavBar";
 import Glimpse from "./components/Glimpse/Glimpse";
-import Signup from "./components/Signup/Signup";
+import Signup from "./component/Signup/Signup";
 import Registration from "./components/registration/registration";
 import { render } from "@testing-library/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 
 const App = () => {
-
   return (
     <div>
-      <NavBar/>
-    <BrowserRouter>
-    
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/registration" element={<Registration/>}/>
-      </Routes>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/registration" element={<Registration />} />
+        </Routes>
       </BrowserRouter>
-        
     </div>
   );
 };
 export default App;
 
-
-      //   <NavBar/>
-      //   <Home/>
-      //   <About />
-      //   <Event />
-      //   <Team /> 
-      //   <Sponsor /> 
-      //   <Glimpse />
-      // <Signup/>
-      // <Registration/>
-      // <Footer />
+//   <NavBar/>
+//   <Home/>
+//   <About />
+//   <Event />
+//   <Team />
+//   <Sponsor />
+//   <Glimpse />
+// <Signup/>
+// <Registration/>
+// <Footer />
