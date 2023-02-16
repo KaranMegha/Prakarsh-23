8559654e14962220cbb5645e365fbdaaa18ffec8
Manavsheth78 <shethmanav2002@gmail.com>
2023-02-16 22:59:46 +0530

    dfwdd

--- src/components/Sponsor/Sponsor.jsx
+++ src/components/Sponsor/Sponsor.jsx
@@ -25,13 +25,37 @@ export const SponserCard = ( ) => {
     </h2>
     <h2 className="front-text mx-auto">Sponser</h2>
   </div>  
+
+  <div className="titlesponser">
+    <h1> Title Sponser</h1>
+
+  <div className=' arrange'>
+      <Card >
+      <Card.Img  src= "assests/Events/cyber.png" />
+      
+    </Card>
+      </div>
+
+<div className='container bg arrange'>
+      <Card className='card  d-flex align-items-center justify-content-center '>
+      <Card.Img className='d-flex align-items-center justify-content-center imgc ' variant="top" src= "assests/Events/cyber.png" />
+      
+    </Card>
+
+</div>
+
+  </div>
+ 
+
+  <div className="poweredsponser">
+    <h1> Powered Sponser</h1>
+
   <div className='container bg'>
       <Card className='card mx-auto d-flex align-items-center justify-content-center'>
       <Card.Img className='mx-auto d-flex align-items-center justify-content-center imgc ' variant="top" src= "assests/Events/cyber.png" />
       
     </Card>
-
-</div>
+      </div>
 
 <div className='container bg'>
       <Card className='card mx-auto d-flex align-items-center justify-content-center'>
@@ -40,6 +64,20 @@ export const SponserCard = ( ) => {
     </Card>
 
 </div>
+
+  </div>
+ 
+
+  <div className="goldsponser">
+    <h1> Gold Sponser</h1>
+
+  <div className='container bg'>
+      <Card className='card mx-auto d-flex align-items-center justify-content-center'>
+      <Card.Img className='mx-auto d-flex align-items-center justify-content-center imgc ' variant="top" src= "assests/Events/cyber.png" />
+      
+    </Card>
+      </div>
+
 <div className='container bg'>
       <Card className='card mx-auto d-flex align-items-center justify-content-center'>
       <Card.Img className='mx-auto d-flex align-items-center justify-content-center imgc ' variant="top" src= "assests/Events/cyber.png" />
@@ -47,6 +85,19 @@ export const SponserCard = ( ) => {
     </Card>
 
 </div>
+
+  </div>
+
+  <div className="silversponser">
+    <h1> Silver Sponser</h1>
+
+  <div className='container bg'>
+      <Card className='card mx-auto d-flex align-items-center justify-content-center'>
+      <Card.Img className='mx-auto d-flex align-items-center justify-content-center imgc ' variant="top" src= "assests/Events/cyber.png" />
+      
+    </Card>
+      </div>
+
 <div className='container bg'>
       <Card className='card mx-auto d-flex align-items-center justify-content-center'>
       <Card.Img className='mx-auto d-flex align-items-center justify-content-center imgc ' variant="top" src= "assests/Events/cyber.png" />
@@ -54,13 +105,12 @@ export const SponserCard = ( ) => {
     </Card>
 
 </div>
-<div className='container bg'>
-      <Card className='card mx-auto d-flex align-items-center justify-content-center'>
-      <Card.Img className='mx-auto d-flex align-items-center justify-content-center imgc ' variant="top" src= "assests/Events/cyber.png" />
-      
-    </Card>
 
-</div>
+  </div>
+ 
+ 
+      
+
 </section>
 
 
